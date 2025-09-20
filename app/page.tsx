1a5d8f7f8d64273e2b94a6e94313488d4c23b51b
ayrtoncatri <ayrtoncatri@gmail.com>
2025-09-19 21:11:28 -0300

    Se agrega componente header y se modifica el componente carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import Carousel from "@/components/carousel/Carousel";
+import Header from "@/components/header/Header";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <Carousel />
+    <div className="main">
+      <Header />
+      <Carousel />
+    </div>
   );
 }
